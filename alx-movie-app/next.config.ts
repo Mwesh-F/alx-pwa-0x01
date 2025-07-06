fb47bffc94c30383f0b783b53a1bb10a8eaa0d6c
Frida Mwende <mwendehfrida@gmail.com>
2025-07-06 22:22:19 +0300

    Update next.config.ts

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -3,8 +3,8 @@ import withPWAInit from "@ducanh2912/next-pwa";
 /** @type {import('next').NextConfig} */
 
 const withPWA = withPWAInit({
-  dest: 'public'
-})
+  dest: "public"
+});
 
 const nextConfig = {
   reactStrictMode: true,
